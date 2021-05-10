d39dd548f13fdadb8aea6154b526e02e1e03638f
Sokolovskiy-Max <78386349+Sokolovskiy-Max@users.noreply.github.com>
2021-05-11 02:31:04 +0300

    Update Vect.java

--- Vect.java
+++ Vect.java
@@ -15,8 +15,8 @@ public class Vect {
         }
     public Vect VMi(Vect a) {
         Vect b=new Vect()  ;
-        b.x=a.x-this.x;
-        b.y=a.y-this.y;
+        b.x=-a.x+this.x;
+        b.y=-a.y+this.y;
         return b;
     }
         public Vect VSp(double  b) {
@@ -27,13 +27,3 @@ public class Vect {
         }
 
 }
-/*
-    double m1= Double.parseDouble(tfm1.getText());
-    double m2= Double.parseDouble(tfm2.getText());
-    double m3= Double.parseDouble(tfm3.getText());
-    double a1= Double.parseDouble(tfm1.getText());
-    double a2= Double.parseDouble(tfm2.getText());
-    double a3= Double.parseDouble(tfm3.getText());
-    double e1= Double.parseDouble(tfm1.getText());
-    double e2= Double.parseDouble(tfm2.getText());
-    double e3= Double.parseDouble(tfm2.getText());*/
